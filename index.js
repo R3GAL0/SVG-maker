d100624cd1967e3bcbb60a76fddd460d8dc5aaaa
R3GAL0 <137714605+R3GAL0@users.noreply.github.com>
2023-09-06 19:15:46 -0400

    finished index.html

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const Circle = require('./lib/circle');
 const Triangle = require('./lib/triangle');
 const Square = require('./lib/square');
+const SVG = require('./lib/svg');
 
 function askQuestions () {
     inquirer
@@ -29,14 +30,37 @@ function askQuestions () {
                 message: 'Enter the svg background color name or value'
             },
         ])
+        .then((data) => {
+            let myLogo;
+            switch(data.shape){
+                case 'circle': 
+                myLogo = new Circle();
+                break;
 
+                case 'triangle':
+                    myLogo = new Triangle();
+                    break;
 
-            // reprompt for bad values
-            // make the svg
-            // print svg to file
+                case 'square':
+                    myLogo = new Square();
+                    break;
+            }
+            myLogo.setColor(data.colorBgnd);
+            const fullSVG = new SVG();
+            fullSVG.setShape(myLogo);
+            fullSVG.setTextColor(data.colorTxt, data.letters);
+            if (data.letters.length > 3) {
+                console.log('too many letters')
+                askQuestions();
+            }
+            writeSVG('thisSVG.svg', fullSVG.makeSVG());
+        });
+}
 
-
-
+function writeSVG (fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        err?console.log(err):console.log('Generated SVG file');
+    })
 }
 
 askQuestions();
